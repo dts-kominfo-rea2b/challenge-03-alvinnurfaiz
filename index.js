ce58c4d3867eb1a8346e983348f9ac2f1739f9ac
alvinnurfaiz <alvinsyahfril@gmail.com>
2022-06-05 20:40:33 +0700

    answer chalenge 3

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // ! JANGAN DIMOFIDIKASI
-const dataBelanjaan = [
-  {
+const dataBelanjaan = [{
     id: "D-10001",
     nama: "Minyak Goreng Delima",
     harga: 25000,
@@ -21,19 +20,29 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = (daftarBelanja) => {
+  return daftarBelanja.map((dataBelanja) => {
+    return (`- ${dataBelanja.nama} x ${dataBelanja.kuantitas}`);
+  });
+};
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+function totalBelanjaan(daftarBelanja) {
+  let totalBelanja = 0;
+  for (const belanja in daftarBelanja) {
+    totalBelanja = totalBelanja + (daftarBelanja[belanja].harga * daftarBelanja[belanja].kuantitas);
+  }
+  return totalBelanja;
+}
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
   console.log("Selamat datang customer");
 
   console.log("Belanjaan Anda adalah:");
-  console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
+  console.log(listBelanjaan ?.(dataBelanjaan) ?.join("\n"));
   console.log(
-    "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan?.(dataBelanjaan)
+    "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan ?.(dataBelanjaan)
   );
 };
 
